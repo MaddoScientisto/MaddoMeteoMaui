ab72fa6d4140d8d8c5c609e459959b9edb1a850d
MaddoScientisto <maddo.science@gmail.com>
2022-06-05 19:00:38 +0200

    Added rows for each value

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -5,16 +5,20 @@ namespace MaddoMeteoMaui.Data;
 public class WeatherForecast
 {
 	public float Winddir { get; set; }
+    [JsonPropertyName("windspeedkmh")]
 	public float WindSpeedKmh { get; set; }
+    [JsonPropertyName("windgustkmh")]
 	public float WindGustKmh { get; set; }
+    [JsonPropertyName("windgustdir")]
 	public float WindGustDir { get; set; }
 	public float Humidity { get; set; }
 	public float Tempf { get; set; }
 	public float Tempc { get; set; }
 	public float TempExtC { get; set; }
+    [JsonPropertyName("rainmm")]
 	public float RainMm { get; set; }
 	public float DailyRainIn { get; set; }
-	public float Pressure { get; set; }
+	public double Pressure { get; set; }
     [JsonPropertyName("batt_lvl")]
 	public float BattLvl { get; set; }
     [JsonPropertyName("light_lvl")]
